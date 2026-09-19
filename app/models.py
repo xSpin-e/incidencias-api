cdb5115de307b33b7c05132d4711577c8cf6af3d
xSpin-e <adrianespindev@gmail.com>
2026-09-19 14:02:27 +0200

    feat: añade entidad Incidencia

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from app.enum import Estado
+from app.database import Base
+
+#BASE ES COMO EL @ENTITY DE JPA
+class IncidenteDB(Base):
+    __tablename__ = "incidentes"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    titulo: Mapped[str]
+    descripcion: Mapped[str]
+    severidad: Mapped[str]
+    estado: Mapped[str] = mapped_column(default=Estado.abierto)
+    cve_id: Mapped[str | None]
