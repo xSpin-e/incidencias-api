907864c24899f6e0cf4e10051978f7896dbfc1f1
xSpin-e <adrianespindev@gmail.com>
2026-09-19 18:04:10 +0200

    feat: añade esquemas de usuario

--- app/schemas.py
+++ app/schemas.py
@@ -15,3 +15,12 @@ class IncidenteCrear(BaseModel):
 class Incidente(IncidenteCrear):
     model_config = ConfigDict(from_attributes=True) #Convierte de INCIDENTE_DB A INCIDENTE_DTO
     id: int
+
+class UsuarioCrear(BaseModel):
+    email: str
+    password: str
+
+class Usuario(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    email: str
