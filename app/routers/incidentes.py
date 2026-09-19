f9d15587f024c5d4b97b62a42e264028ad964bf2
xSpin-e <adrianespindev@gmail.com>
2026-09-19 14:39:35 +0200

    feat: añade post en api con BD

--- app/routers/incidentes.py
+++ app/routers/incidentes.py
@@ -1,15 +1,14 @@
 from fastapi import APIRouter, HTTPException
 from app.schemas import Incidente, IncidenteCrear
+from app.services import incidente_service
+from app.database import DbSession
 
 router = APIRouter(prefix="/incidentes", tags=["incidentes"])
 incidentes_db: dict[int, Incidente] = {}
 
 @router.post("", response_model=Incidente, status_code=201)
-def crear_incidente(datos: IncidenteCrear):
-    nuevo_id = len(incidentes_db) + 1
-    incidente = Incidente(id=nuevo_id, **datos.model_dump()) #convierte la información en un diccionario
-    incidentes_db[nuevo_id] = incidente
-    return incidente
+def crear_incidente(datos: IncidenteCrear, db: DbSession):
+    return incidente_service.crear_incidente(db, datos)
 
 @router.get("", response_model=list[Incidente])
 def obtener_incidentes():
