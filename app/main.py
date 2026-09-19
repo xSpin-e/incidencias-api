db3a0624fc75209e76cdf79ab980162a2f741752
xSpin-e <adrianespindev@gmail.com>
2026-09-19 18:17:31 +0200

    feat: añade enlazado de ruta en main

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.routers import incidentes
 from app.database import init_db
+from app.routers import usuarios
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -10,6 +11,7 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(incidentes.router)
+app.include_router(usuarios.router)
 
 
 @app.get("/health")
