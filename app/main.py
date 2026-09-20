c4a268679cc629ae227193b1704d782bda9aa627
xSpin-e <adrianespindev@gmail.com>
2026-09-20 15:50:25 +0200

    feat: añade CORS

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from app.routers import incidentes
 from app.routers import usuarios
 from app.database import SessionLocal, init_db
 from app.services import usuario_service
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,6 +15,15 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://scorpion.adrianespin.com",
+        "http://localhost:5173",
+    ],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(incidentes.router)
 app.include_router(usuarios.router)
 
